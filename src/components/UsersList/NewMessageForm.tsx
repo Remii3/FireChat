0c1451faff3063268117d2355473061792f94458
Remi <remek778790@gmail.com>
2024-04-20 18:10:30 +0200

    add emojis

--- src/components/UsersList/NewMessageForm.tsx
+++ src/components/UsersList/NewMessageForm.tsx
@@ -3,13 +3,14 @@
 import React, { FormEvent, useEffect, useState } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { Loader2, Send } from "lucide-react";
+import { Loader2, Send, Smile } from "lucide-react";
 import { useAddMessage } from "@/hooks/useMessages";
 import { User } from "@/types/user";
+import EmojiPicker from "emoji-picker-react";
 
 const NewMessageForm = ({ selectedUser }: { selectedUser: User }) => {
   const [newMessage, setNewMessage] = useState("");
-
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
   const { mutate, isPending, isSuccess } = useAddMessage({
     newMessageText: newMessage,
     recipientId: selectedUser.uid,
@@ -26,24 +27,47 @@ const NewMessageForm = ({ selectedUser }: { selectedUser: User }) => {
   }, [isSuccess]);
 
   return (
-    <form onSubmit={sendMessage} className="flex space-x-2 mt-4 basis-10 px-4">
-      <Input
-        value={newMessage}
-        onChange={(e) => setNewMessage(e.target.value)}
+    <div className="relative">
+      <EmojiPicker
+        style={{ position: "absolute", bottom: "60px", left: "16px" }}
+        open={showEmojiPicker}
+        onEmojiClick={(a, b) => setNewMessage((prev) => prev + a.emoji)}
       />
-      <Button
-        variant={"default"}
-        type="submit"
-        disabled={newMessage.trim().length <= 0}
-        className="transition"
+
+      <form
+        onSubmit={sendMessage}
+        className="flex relative space-x-2 mt-4 basis-10 px-4"
       >
-        {isPending ? (
-          <Loader2 className="h-4 w-4 animate-spin"></Loader2>
-        ) : (
-          <Send className="h-4 w-4" />
-        )}
-      </Button>
-    </form>
+        <Input
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          className="pl-10"
+        />
+        <button
+          type="button"
+          aria-label="Show emoji picker"
+          onClick={() => setShowEmojiPicker((prev) => !prev)}
+        >
+          <Smile
+            className={`${
+              showEmojiPicker && "rotate-45"
+            } h-6 w-6 absolute top-2 left-6 cursor-pointer transition hover:rotate-45`}
+          />
+        </button>
+        <Button
+          variant={"default"}
+          type="submit"
+          disabled={newMessage.trim().length <= 0}
+          className="transition"
+        >
+          {isPending ? (
+            <Loader2 className="h-4 w-4 animate-spin"></Loader2>
+          ) : (
+            <Send className="h-4 w-4" />
+          )}
+        </Button>
+      </form>
+    </div>
   );
 };
 
